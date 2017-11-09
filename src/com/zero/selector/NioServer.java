d096c18ef01196c2133e27b27f96310603290762
scvzerng <443683059a>
2017-11-09 17:05:36 +0800

    处理客户端关闭事件  包括强制关机

--- src/com/zero/selector/NioServer.java
+++ src/com/zero/selector/NioServer.java
@@ -10,29 +10,29 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 public class NioServer extends Thread {
 
-    public static Map<SocketChannel,StringBuilder> channelData = new ConcurrentHashMap<>();
+    public static Map<SocketChannel, StringBuilder> channelData = new ConcurrentHashMap<>();
+
     public NioServer() throws IOException {
     }
 
     public static void main(String[] args) throws IOException {
         new NioServer().start();
     }
+
     Selector selector = Selector.open();
 
-    private void initServer(Consumer<Set<SelectionKey>> events){
+    private void initServer(Consumer<Set<SelectionKey>> events) {
         try {
             ServerSocketChannel serverSocket = ServerSocketChannel.open();
             serverSocket.bind(new InetSocketAddress(9999));
             serverSocket.configureBlocking(false);
-            serverSocket.register(selector,SelectionKey.OP_ACCEPT);
-            while (true){
-                if(selector.select()==0){
+            serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+            while (true) {
+                if (selector.select() == 0) {
                     continue;
                 }
                 events.accept(selector.selectedKeys());
@@ -44,31 +44,29 @@ public class NioServer extends Thread {
 
     @Override
     public void run() {
-        initServer(events->{
+        initServer(events -> {
             Iterator<SelectionKey> keyIterable = events.iterator();
 
-                while (keyIterable.hasNext()){
+            while (keyIterable.hasNext()) {
                 SelectionKey selectionKey = keyIterable.next();
-                    keyIterable.remove();
+                keyIterable.remove();
+                if (selectionKey.isValid()) {
                     try {
 
-                        if(selectionKey.isAcceptable()){
-                        handlerConnect(selectionKey);
+                        if (selectionKey.isAcceptable()) {
+                            handlerConnect(selectionKey);
 
-                }
+                        }
 
-                if(selectionKey.isReadable()){
-                        handlerReader(selectionKey);
+                        if (selectionKey.isReadable()) {
+                            handlerReader(selectionKey);
 
-                }
+                        }
                     } catch (IOException e) {
-                        close((SocketChannel) selectionKey.channel());
+                        //客户端关闭强制关闭是通过读取数据时抛出异常来表示
+                        close((SocketChannel) selectionKey.channel(), 1);
                     }
-
-                if(!selectionKey.isValid()){
-                    System.out.println("close");
                 }
-
 
             }
 
@@ -78,6 +76,7 @@ public class NioServer extends Thread {
 
     /**
      * 处理读取事件
+     *
      * @param event
      * @throws IOException
      */
@@ -86,39 +85,43 @@ public class NioServer extends Thread {
         SocketChannel channel = (SocketChannel) event.channel();
         StringBuilder result = getOrDefault(channel);
         int count;
-        while ((count=channel.read(buffer))>0){
+        while ((count = channel.read(buffer)) > 0) {
             buffer.flip();
             byte[] contents = new byte[buffer.limit()];
-            buffer.get(contents,0,buffer.limit());
+            buffer.get(contents, 0, buffer.limit());
             result.append(new String(contents));
             buffer.clear();
-            System.out.println("receiving:"+result.toString());
+            System.out.println("receiving:" + result.toString());
         }
-        if(count==-1){
-          close(channel);
+        if (count == -1) {
+            //数据读取完毕  客户端正常退出
+            close(channel, count);
         }
-
 
 
     }
-   private void close(SocketChannel channel) {
-       try {
-           channel.close();
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
-   }
+
+    private void close(SocketChannel channel, int state) {
+        try {
+            channel.close();
+            System.out.println((state > 0 ? "客户端强制断开" : "") + "all received:" + channelData.get(channel));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * 获取通道结果存储介质
+     *
      * @param channel
      * @return
      */
-    private StringBuilder getOrDefault(SocketChannel channel){
+    private StringBuilder getOrDefault(SocketChannel channel) {
         StringBuilder builder = channelData.get(channel);
-        if(builder==null){
-            synchronized (this){
+        if (builder == null) {
+            synchronized (this) {
                 builder = new StringBuilder();
-                channelData.put(channel,builder);
+                channelData.put(channel, builder);
                 builder = channelData.get(channel);
             }
         }
@@ -127,15 +130,16 @@ public class NioServer extends Thread {
 
     /**
      * 处理连接事件
+     *
      * @param key
      * @throws IOException
      */
     private void handlerConnect(SelectionKey key) throws IOException {
         ServerSocketChannel channel = (ServerSocketChannel) key.channel();
         SocketChannel client = channel.accept();
-        if(client!=null){
+        if (client != null) {
             client.configureBlocking(false);
-            client.register(selector,SelectionKey.OP_READ);
+            client.register(selector, SelectionKey.OP_READ);
         }
     }
 
